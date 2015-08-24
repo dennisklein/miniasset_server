255f06dfc373ac202faad7da5847ae41fb007917
Dennis Klein <d.klein@gsi.de>
2015-08-24 21:41:20 +0200

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+minicube = Datacenter.create name: 'Minicube', rack_prefix: 'TES'
+
+(1..4).each do |row|
+  (1..12).each do |cell|
+    DatacenterRackLocation.create datacenter: minicube, rack: DatacenterRack.create(custom_prefix: 'A_'),
+                                  floor: 1, row: row, cell: cell
+  end
+end
