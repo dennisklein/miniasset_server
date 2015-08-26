e8224e6ed53575cb98b5f2b0d236ee6c0c90747e
Dennis Klein <d.klein@gsi.de>
2015-08-26 21:40:52 +0200

    refactor and document Datacenter model

--- app/models/datacenter.rb
+++ app/models/datacenter.rb
@@ -1,24 +1,31 @@
+# Datacenter model
+#
+# A datacenter has many racks, each at a certain
+# unique position (floor, row, cell). The attributes
+# :rack_prefix, :max_floors, :max_rows_per_floor and
+# :max_cells_per_row are used for generating rack
+# labels in the DatacenterRack model.
+
 class Datacenter < ActiveRecord::Base
   include RocketPants::Cacheable
+
   DEFAULTS = {
     max_floors: 16,
     max_rows_per_floor: 16,
     max_cells_per_row: 16
   }
 
-  validates :name, presence: true, length: { maximum: 64 }
-
-  validates :rack_prefix, presence: true, length: { maximum: 5 }
-
-  has_many :rack_locations, class_name: 'DatacenterRackLocation'
-  has_many :racks, through: :rack_locations
-
-  validates :max_floors, presence: true
-  validates :max_rows_per_floor, presence: true
-  validates :max_cells_per_row, presence: true
-
   def initialize(attributes = {})
     attr_with_defaults = DEFAULTS.merge(attributes)
     super(attr_with_defaults)
   end
+
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :rack_prefix, presence: true, length: { maximum: 5 }
+  validates :max_floors, presence: true
+  validates :max_rows_per_floor, presence: true
+  validates :max_cells_per_row, presence: true
+
+  has_many :rack_locations, class_name: 'DatacenterRackLocation'
+  has_many :racks, through: :rack_locations
 end
